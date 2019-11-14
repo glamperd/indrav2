6d80f0c9d3e61658ea700bd0d55d021dd1f3c3fa
Geoff Lamperd <glamperd@gmail.com>
2019-11-14 15:58:16 +1100

    Babel in webpack config

--- modules/client/webpack.config.js
+++ modules/client/webpack.config.js
@@ -2,13 +2,24 @@ const path = require('path');
 //const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  devtool: 'source-map',
   entry: './src/index.ts',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+            presets: ['env', 'react', 'es2015'],
+            plugins: ['transform-class-properties']
+        }
       },
     ],
   },
